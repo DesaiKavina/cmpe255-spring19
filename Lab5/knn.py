e14696218d8600636de4acab13177e8e5bbe4964
DesaiKavina <kavina.desai@sjsu.edu>
2019-02-26 23:18:34 -0800

    Lab5 work

---
+++ Lab5/knn.py
@@ -0,0 +1,62 @@
+import data
+import linear_algebra
+from data import *
+from collections import Counter
+from linear_algebra import distance
+from statistics import mean
+import math, random
+import matplotlib.pyplot as plt
+
+def majority_vote(labels):
+    """assumes that labels are ordered from nearest to farthest"""
+    vote_counts = Counter(labels)
+    winner, winner_count = vote_counts.most_common(1)[0]
+    num_winners = len([count
+                       for count in vote_counts.values()
+                       if count == winner_count])
+
+    if num_winners == 1:
+        return winner                     # unique winner, so return it
+    else:
+        return majority_vote(labels[:-1]) # try again without the farthest
+
+
+def knn_classify(k, labeled_points, new_point):
+    """each labeled point should be a pair (point, label)"""
+
+    # order the labeled points from nearest to farthest
+    by_distance = sorted(labeled_points,
+                         key=lambda point_label: distance(point_label[0], new_point))
+
+    # find the labels for the k closest
+    k_nearest_labels = [label for _, label in by_distance[:k]]
+
+    # and let them vote
+    return majority_vote(k_nearest_labels)
+
+
+def predict_preferred_language_by_city(k_values, cities):
+    """
+    TODO
+    predicts a preferred programming language for each city using above knn_classify() and 
+    counts if predicted language matches the actual language.
+    Finally, print number of correct for each k value using this:
+    print(k, "neighbor[s]:", num_correct, "correct out of", len(cities))
+    """
+    print("\nInside the function")
+    for i in k_values:
+        print("For the value k = ",i)
+        counter = 0
+        for j in range(len(cities)):
+            copy_list = list(cities)
+            copy_list.pop(j)
+            val = knn_classify(i, copy_list, (cities[j][0][0],cities[j][0][1]))
+            if val == cities[j][1]:
+                counter = counter + 1
+        print("\n",i, "neighbor[s]:", counter, "correct out of", len(cities))   
+
+if __name__ == "__main__":
+    k_values = [1, 3, 5, 7]
+    # TODO
+    # Import cities from data.py and pass it into predict_preferred_language_by_city(x, y).
+    predict_preferred_language_by_city(k_values, cities)
---
+++ Lab5/plots.py
@@ -0,0 +1,83 @@
+import data
+import linear_algebra
+import knn
+from knn import *
+from data import *
+from collections import Counter
+from linear_algebra import distance
+from statistics import mean
+import math, random
+import matplotlib.pyplot as plt
+
+def plot_state_borders(plt, color='0.8'):
+    pass
+
+def plot_cities(cities):
+
+    # key is language, value is pair (longitudes, latitudes)
+    plots = { "Java" : ([], []), "Python" : ([], []), "R" : ([], []) }
+    # we want each language to have a different marker and color
+    markers = { "Java" : "o", "Python" : "s", "R" : "^" }
+    colors  = { "Java" : "r", "Python" : "b", "R" : "g" }
+
+    for (longitude, latitude), language in cities:
+        plots[language][0].append(longitude)
+        plots[language][1].append(latitude)
+
+    # create a scatter series for each language
+    for language, (x, y) in plots.items():
+        plt.scatter(x, y, color=colors[language], marker=markers[language],
+                          label=language, zorder=10)
+
+    plot_state_borders(plt)    # assume we have a function that does this
+
+    plt.legend(loc=0)          # let matplotlib choose the location
+    plt.axis([-130,-60,20,55]) # set the axes
+    plt.title("Favorite Programming Languages")
+    plt.show()
+
+
+def classify_and_plot_grid(cities, k=1):
+    """
+    TODO
+    Classify and plot for Python, Java, and R languages.
+    """
+    plots = { "Java" : ([], []), "Python" : ([], []), "R" : ([], []) }
+    markers = { "Java" : "o", "Python" : "s", "R" : "^" }
+    colors  = { "Java" : "r", "Python" : "b", "R" : "g" }
+
+    # Predict preferred language for each city using knn_classify() from knn.py.
+    # longitude range (-130, -60)
+    # latitude in range (20, 55)
+    # Save the coordinate of prediction result in plots variable.
+    # TODO
+
+    for lon in range(-130,-60):
+        for lat in range(20, 55):
+            # coord = (lon,lat)
+            val = knn_classify(k, cities, (lon,lat))
+            # print(val)
+            plots[val][0].append(lon)
+            plots[val][1].append(lat)
+            
+    # TODO
+    # create a scatter series for each language
+    for language, (x, y) in plots.items():
+        plt.scatter(x, y, color=colors[language], marker=markers[language],
+                          label=language, zorder=10)
+
+    plot_state_borders(plt)    # assume we have a function that does this
+
+    plt.legend(loc=0)          # let matplotlib choose the location
+    plt.axis([-130,-60,20,55]) # set the axes
+    plt.title("Favorite Programming Languages")
+    plt.show()
+    
+
+
+if __name__ == "__main__":
+    # TODO import cities from data.py
+    plot_cities(cities)
+    classify_and_plot_grid(cities)
+    classify_and_plot_grid(cities, 3)
+    classify_and_plot_grid(cities, 5)
